a3c4949f44ee4729eedcc75fa1296b18b1f7370a
Barun Prasad <barun.prasad@hotmail.com>
2024-08-21 09:57:06 +0530

    feat(Colors): add magenta color

---
+++ packages/colors/src/lib/magenta.ts
@@ -0,0 +1,14 @@
+import { ColorType } from './types';
+
+export const magenta: ColorType = {
+  50: '#FFEBF7',
+  100: '#FFD7EF',
+  200: '#FEB3E1',
+  300: '#FE8BD2',
+  400: '#FD62C2',
+  500: '#FD3DB5',
+  600: '#F7029A',
+  700: '#BB0274',
+  800: '#7E014E',
+  900: '#3D0126',
+};
