97c25ec703699851130223a3d09e3633a8792dc5
Barun Prasad <barun.prasad@hotmail.com>
2024-08-29 15:42:22 +0530

    fix(icons): remove fill from path elements of svg to apply the currentColor

--- packages/icons/src/solid/ArrowTurnLeftDownIcon.tsx
+++ packages/icons/src/solid/ArrowTurnLeftDownIcon.tsx
@@ -11,7 +11,6 @@ export default function ArrowTurnLeftDownIcon(props: IconProps) {
       {...props}
     >
       <path
-        fill="#000"
         fillRule="evenodd"
         d="M20.24 3.75a.75.75 0 0 1-.75.75H8.989v13.939l2.47-2.47a.75.75 0 1 1 1.06 1.061l-3.75 3.75a.75.75 0 0 1-1.06 0l-3.751-3.75a.75.75 0 1 1 1.06-1.06l2.47 2.469V3.75a.75.75 0 0 1 .75-.75H19.49a.75.75 0 0 1 .75.75"
         clipRule="evenodd"
--- packages/icons/src/solid/DivideIcon.tsx
+++ packages/icons/src/solid/DivideIcon.tsx
@@ -11,7 +11,6 @@ export default function DivideIcon(props: IconProps) {
       {...props}
     >
       <path
-        fill="#000"
         fillRule="evenodd"
         d="M10.874 5.248a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0m-7.125 6.75a.75.75 0 0 1 .75-.75h15a.75.75 0 0 1 0 1.5h-15a.75.75 0 0 1-.75-.75m7.125 6.753a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0"
         clipRule="evenodd"
